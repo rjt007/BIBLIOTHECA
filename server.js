16d1db1bc63434c166a3eb73bca1c865a3c64aa5
Rajat Agrawal <rajatagrawal007.ra@gmail.com>
2021-11-30 10:53:29 +0530

    Add author route

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-if (process.env.NODE_ENV!=='production') {
-    require('dotenv').config();
-}
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const expressLayout = require('express-ejs-layouts');
 const indexRoute = require('./routes/index');
-
+const authorRoute = require('./routes/authors');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views',__dirname+'/views');
@@ -14,7 +12,9 @@ app.set('layout','layouts/layout');
 
 app.use(expressLayout);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
 app.use('/',indexRoute);
+app.use('/authors',authorRoute);
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL);
@@ -23,6 +23,7 @@ const db = mongoose.connection;
 db.on('error', error=>console.error(error));
 db.once('open',()=>console.log('Connected to database..'));
 
-app.listen(process.env.PORT || 4000, ()=>{
+const PORT = process.env.PORT;
+app.listen(PORT, ()=>{
     console.log("Server is listening on port 4000...");
 })
