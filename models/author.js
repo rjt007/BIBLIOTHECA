147ebd1a962d6d2a7e98c40353187d9e5d51e5dc
Rajat Agrawal <rajatagrawal007.ra@gmail.com>
2021-12-09 19:31:55 +0530

    setup edit/view/delete routes for author route

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Book = require('./book');
 
 const authorSchema = mongoose.Schema({
     name:{
@@ -7,4 +8,18 @@ const authorSchema = mongoose.Schema({
     }
 });
 
+authorSchema.pre('remove',function(next){
+    Book.find({author:this.id},(err,books)=>{
+        if(err){
+            next(err);
+        }
+        else if(books.length>0){
+            next(new Error('This Author still has books!'));
+        }
+        else
+        {
+            next();
+        }
+    })
+})
 module.exports = mongoose.model('Author',authorSchema);
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const app = express();
 const expressLayout = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const indexRoute = require('./routes/index');
 const authorRoute = require('./routes/authors');
 const bookRoute = require('./routes/books');
@@ -17,6 +18,7 @@ app.set('layout','layouts/layout');
 
 app.use(expressLayout);
 app.use(express.static('public'));
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
 app.use('/',indexRoute);
 app.use('/authors',authorRoute);
